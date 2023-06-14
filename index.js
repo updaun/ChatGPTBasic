9572d821b948c8960a30563db0b4bf30fe22d817
updaun <updauney@daum.net>
2023-06-14 22:59:15 +0900

    [feat] openai api examples

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const { Configuration, OpenAIApi } = require("openai");
+const apiKey = "api-key-here";
 
+const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
-  apiKey: "api-key-here",
+  apiKey: apiKey,
 });
 const openai = new OpenAIApi(configuration);
-
 async function apiCall() {
-    const completion = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: "Hello world",
-    });
-    console.log(completion.data.choices[0].text);
-    return completion;
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "Say this is a test",
+    max_tokens: 7,
+    temperature: 0,
+  });
+  console.log(response.data.choices[0].message);
 }
-
 apiCall();
